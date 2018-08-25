97f7a78581688e9589868f0d7a7ea211a97d012d
Jose David Giraldo Mosquera <josedavidgm1995@gmail.com>
2018-08-25 16:55:18 -0500

    Add title

--- main.js
+++ main.js
@@ -2,5 +2,6 @@
     let body = document.querySelector("body");
     console.log("Hi!");
     
-    body.innerHTML = ``;
+    body.innerHTML = `<h1>Click and drag</h1>
+                        <p>By Jose David GM - 09/2018</p>`;
 })();
