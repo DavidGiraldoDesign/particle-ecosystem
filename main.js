e7132435af474dc058a98142e6986aa7e6a58f1e
Jose David Giraldo Mosquera <josedavidgm1995@gmail.com>
2018-08-25 17:20:09 -0500

    Center text with flex-box

--- main.js
+++ main.js
@@ -2,6 +2,12 @@
     let body = document.querySelector("body");
     console.log("Hi!");
     
-    body.innerHTML = `<h1>Click and drag</h1>
-                        <p>By Jose David GM - 09/2018</p>`;
+    body.innerHTML = `<div class="container disable-select">
+                        <h1>JD</h1>                    
+                        <h2>Click and drag</h2>
+                        <br>
+                     </div> 
+                       <p>By Jose David GM - 09/2018</p>
+                        `;
+                        
 })();
