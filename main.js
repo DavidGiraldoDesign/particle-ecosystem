2cfb436daa421d6183496a4381b4506012786800
Jose David <josedavidgm1995@gmail.com>
2018-08-26 15:18:22 -0500

    Add socialNetwork information

--- main.js
+++ main.js
@@ -3,11 +3,20 @@
     console.log("Hi!");
     
     body.innerHTML = `<div class="container disable-select">
-                        <h1>JD</h1>                    
-                        <h2>Click and drag</h2>
-                        <br>
+                         <h1>JD</h1>                    
+                         <h2>Click and drag</h2>
+                         <br>
                      </div> 
-                       <p>By Jose David GM - 09/2018</p>
+                     <div id="title">
+                         <p>Particle ecosystem</p>
+                     </div>
+                     <div id="info">
+                         <p>By Jose David GM - 09/2018</p>
+                         <p>Follow me on 
+                         <a href="https://www.behance.net/JoseDavidGiraldoM" target=" _blank">Behance</a> 
+                         / <a href="https://www.instagram.com/_david.giraldo/" target=" _blank">Instagram<a></p>
+                     </div>
+                       
                         `;
                         
 })();
